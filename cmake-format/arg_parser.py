1d67b95cf93bfb61e1f9109b33ee50867118173f
Genci Berisha <me@genciberisha.dev>
2024-12-01 21:23:16 +0100

    Refactor: Improve argument parser organization

--- cmake-format/arg_parser.py
+++ cmake-format/arg_parser.py
@@ -2,72 +2,60 @@ import argparse
 from distutils.util import strtobool
 
 
+def _split_list(value):
+    """Split a string into a list of paths/patterns."""
+    return [item for line in value.split("\n") for item in line.split()]
+
+
 def parse_cmake_format_args():
+    """Parse and return command line arguments for cmake-format."""
     parser = argparse.ArgumentParser(
         description="Parse cmake-format command line arguments"
     )
 
-    parser.add_argument(
-        "--in-place", action="store_true", help="Modify input file(s) in place"
-    )
-    parser.add_argument(
-        "--check", action="store_true", help="Check formatting without making changes"
-    )
-    parser.add_argument(
+    # Formatting options
+    formatting_group = parser.add_argument_group('Formatting Options')
+    formatting_group.add_argument(
         "--line-width",
         type=int,
         metavar="WIDTH",
         help="Set maximum line width for formatting",
     )
-    parser.add_argument(
-        "-e",
-        "--exclude",
-        type=lambda x: [pattern for line in x.split("\n") for pattern in line.split()],
-        default=[],
-    )
-    parser.add_argument(
+    formatting_group.add_argument(
         "--tab-size",
         type=int,
         metavar="SIZE",
         help="Set the number of spaces for indentation",
     )
-    parser.add_argument(
-        "--no-tab-indentation", action="store_true", help="Use spaces for indentation"
-    )
-    parser.add_argument(
-        "--comment-style", metavar="STYLE", help="Specify comment style for formatting"
-    )
-    parser.add_argument(
-        "--remove-empty", action="store_true", help="Remove empty lines"
-    )
-    parser.add_argument(
-        "--color",
-        default="auto",
-        choices=["auto", "always", "never"],
-        help="show colored diff (default: auto)",
-    )
-    parser.add_argument(
-        "-q",
-        "--quiet",
+    formatting_group.add_argument(
+        "--no-tab-indentation",
         action="store_true",
-        help="disable output, useful for the exit code",
+        help="Use spaces for indentation"
     )
-    parser.add_argument(
-        "-j",
-        metavar="N",
-        type=int,
-        default=0,
-        help="run N clang-format jobs in parallel" " (default number of cpus + 1)",
+    formatting_group.add_argument(
+        "--comment-style",
+        metavar="STYLE",
+        help="Specify comment style for formatting"
     )
-
-    parser.add_argument(
-        "--config",
-        type=lambda x: bool(strtobool(x)),
-        default=False,
-        help="Formatting style to use (default: file)",
+    formatting_group.add_argument(
+        "--remove-empty",
+        action="store_true",
+        help="Remove empty lines"
     )
 
-    parser.add_argument(
+    # Operation mode options
+    mode_group = parser.add_argument_group('Operation Mode')
+    mode_group.add_argument(
+        "--in-place",
+        action="store_true",
+        help="Modify input file(s) in place"
+    )
+    mode_group.add_argument(
+        "--check",
+        action="store_true",
+        help="Check formatting without making changes"
+    )
+    mode_group.add_argument(
         "-i",
         "--inplace",
         type=lambda x: bool(strtobool(x)),
@@ -75,11 +63,50 @@ def parse_cmake_format_args():
         help="Just fix files (`clang-format -i`) instead of returning a diff",
     )
 
-    parser.add_argument(
-        "files",
-        type=lambda x: [path for line in x.split("\n") for path in line.split()],
-        metavar="file",
+    # Output options
+    output_group = parser.add_argument_group('Output Options')
+    output_group.add_argument(
+        "--color",
+        default="auto",
+        choices=["auto", "always", "never"],
+        help="show colored diff (default: auto)",
+    )
+    output_group.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="disable output, useful for the exit code",
     )
 
-    args = parser.parse_args()
-    return args
+    # Processing options
+    processing_group = parser.add_argument_group('Processing Options')
+    processing_group.add_argument(
+        "-j",
+        metavar="N",
+        type=int,
+        default=0,
+        help="run N clang-format jobs in parallel (default: number of CPUs + 1)",
+    )
+    processing_group.add_argument(
+        "-e",
+        "--exclude",
+        type=_split_list,
+        default=[],
+        help="Folders to exclude from formatting check",
+    )
+    processing_group.add_argument(
+        "--config",
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        help="Use .cmake-format style configuration",
+    )
+
+    # Positional arguments
+    parser.add_argument(
+        "files",
+        type=_split_list,
+        metavar="file",
+        help="Files or directories to process",
+    )
+
+    return parser.parse_args()
